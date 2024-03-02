0861c0bc436e7878efea01022a97d7703b7d55f2
pranavkareliya <157805189+pranavkareliya@users.noreply.github.com>
2024-03-02 16:38:24 +0530

    Add files via upload

---
+++ Module 5/1. Basic of Database/1.sql
@@ -0,0 +1,11 @@
+/*
+1. What do you understand by Database.
+*/
+
+/*
+
+ANS :-
+
+	A database is an organized collection of structured information, or data, typically stored
+	electronically in a computer system and combination of row and column.
+*/
---
+++ Module 5/1. Basic of Database/3.sql
@@ -0,0 +1,11 @@
+/*
+3. What is difference between DBMS and RDBMS?
+*/
+
+/*
+ANS :-
+
+	RDBMS stores data in the form of tables, whereas DBMS stores data in the form of files. 
+	Single users are supported by DBMS, whereas multiple users are supported by RDBMS. 
+	Client-server architecture is not supported by DBMS, although it is supported by RDBMS.
+*/
